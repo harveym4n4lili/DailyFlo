b754b3c7593f5c033699eb4f53b26923b86b8cd2
sushi1e <harveymanalililili@gmail.com>
2025-06-22 21:41:44 +0100

    Made changes to custom user model, and user serializers.

--- backend/dailyflo/apps/accounts/serializers.py
+++ backend/dailyflo/apps/accounts/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate, get_user_model
 from .models import CustomUser
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class LoginCustomUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        user = authenticate(
+            username = attrs.get('username'),
+            password = attrs.get('password')
+        )
+        if not user:
+            raise serializers.ValidationError("Invalid username or password.")
+        attrs['user'] = user
+        return attrs
+
+class RegisterCustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min=8)
+    
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'bio']
+        fields = ['id', 'username', 'password', 'first_name', 'last_name',
+                  'display_name', 'bio']
+        
+    def create(self, validated_data):
+        return CustomUser.objects.create(**validated_data)
