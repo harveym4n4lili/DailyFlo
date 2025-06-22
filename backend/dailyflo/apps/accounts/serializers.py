bf75655bfc010470e6f64f6956570ac3fba7a27a
sushi1e <harveymanalililili@gmail.com>
2025-06-22 23:08:51 +0100

    Implemented basic JWT token obtain feature, covering login endpoint, implemented register endpoint, improved accounts serializers.

--- backend/dailyflo/apps/accounts/serializers.py
+++ backend/dailyflo/apps/accounts/serializers.py
@@ -2,19 +2,29 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate, get_user_model
 from .models import CustomUser
 
-class LoginCustomUserSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'password', 'first_name', 'last_name',
+                  'full_name', 'display_name', 'bio']
+        
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
     
-    def validate(self, attrs):
-        user = authenticate(
-            username = attrs.get('username'),
-            password = attrs.get('password')
-        )
-        if not user:
-            raise serializers.ValidationError("Invalid username or password.")
-        attrs['user'] = user
-        return attrs
+# Login serializer not needed as we are using SimpleJWT built in methods for login
+# class LoginCustomUserSerializer(serializers.Serializer):
+#     username = serializers.CharField()
+#     password = serializers.CharField(write_only=True)
+    
+#     def validate(self, attrs):
+#         user = authenticate(
+#             username = attrs.get('username'),
+#             password = attrs.get('password')
+#         )
+#         if not user:
+#             raise serializers.ValidationError("Invalid username or password.")
+#         attrs['user'] = user
+#         return attrs
 
 class RegisterCustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, min=8)
@@ -25,4 +35,8 @@ class RegisterCustomUserSerializer(serializers.ModelSerializer):
                   'display_name', 'bio']
         
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
+        password = validated_data.pop('password')
+        user = user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
