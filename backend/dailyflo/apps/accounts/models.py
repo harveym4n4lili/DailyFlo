505c956b1a2e02be737b129832e69fed380a48a4
sushi1e <harveymanalililili@gmail.com>
2025-06-22 21:41:44 +0100

    Made changes to custom user model, and user serializers.

--- backend/dailyflo/apps/accounts/models.py
+++ backend/dailyflo/apps/accounts/models.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=100, blank=True, null=True)
-    first_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
