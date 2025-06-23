b60a2ca5669b3235d40eba7f5f79f791f93b55c5
sushi1e <harveymanalililili@gmail.com>
2025-06-23 12:00:12 +0100

    Implemented list view and detail view for RecurringTask

--- backend/dailyflo/apps/tasks/urls.py
+++ backend/dailyflo/apps/tasks/urls.py
@@ -4,4 +4,6 @@ from .views import TaskListAPIView, TaskDetailAPIView
 urlpatterns = [
     path('', TaskListAPIView.as_view(), name='tasks-list'),
     path('<int:pk>/', TaskDetailAPIView.as_view(), name='tasks-detail'),
+    path('recurring', TaskListAPIView.as_view(), name='tasks-list'),
+    path('recurring/<int:pk>/', TaskDetailAPIView.as_view(), name='tasks-detail'),
 ]
--- backend/dailyflo/apps/tasks/views.py
+++ backend/dailyflo/apps/tasks/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.shortcuts import get_object_or_404
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, RecurringTask
+from .serializers import TaskSerializer, RecurringTaskSerializer
 
 class TaskListAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -39,6 +39,40 @@ class TaskDetailAPIView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class RecurringTaskDetailAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get_object(self, pk):
+        from django.shortcuts import get_object_or_404
+        return get_object_or_404(RecurringTask, pk=pk)
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = RecurringTaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = RecurringTaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class RecurringTaskListAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self):
+        tasks = RecurringTask.objects.all()
+        serializer = RecurringTaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RecurringTaskSerializer(data=request.data) # send input data to serializer
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     # implement soft delete
     # def delete(self, request, pk):
     #     item = self.get_object(pk)
