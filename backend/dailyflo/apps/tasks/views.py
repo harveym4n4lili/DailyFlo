be4308b9682dd5e2efdf645c76dfd385e99d4c71
sushi1e <harveymanalililili@gmail.com>
2025-06-23 12:15:56 +0100

    Written soft delete views for Task and RecurringTask

--- backend/dailyflo/apps/tasks/views.py
+++ backend/dailyflo/apps/tasks/views.py
@@ -11,7 +11,7 @@ class TaskListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = TaskSerializer(data=request.data) # send input data to serializer
@@ -36,7 +36,24 @@ class TaskDetailAPIView(APIView):
         serializer = TaskSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ToggleDeleteTaskAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get_object(self, pk):
+        from django.shortcuts import get_object_or_404
+        return get_object_or_404(Task, pk=pk)
+    
+    def patch(self, request, pk):
+        task = self.get_object(pk)
+        task.soft_deleted = not task.soft_deleted
+        task.save()
+        
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class RecurringTaskDetailAPIView(APIView):
@@ -55,7 +72,7 @@ class RecurringTaskDetailAPIView(APIView):
         serializer = RecurringTaskSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class RecurringTaskListAPIView(APIView):
@@ -64,7 +81,7 @@ class RecurringTaskListAPIView(APIView):
     def get(self):
         tasks = RecurringTask.objects.all()
         serializer = RecurringTaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = RecurringTaskSerializer(data=request.data) # send input data to serializer
@@ -73,8 +90,19 @@ class RecurringTaskListAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    # implement soft delete
-    # def delete(self, request, pk):
-    #     item = self.get_object(pk)
-    #     item.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+class ToggleDeleteRecurringTaskAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get_object(self, pk):
+        from django.shortcuts import get_object_or_404
+        return get_object_or_404(RecurringTask, pk=pk)
+    
+    def patch(self, request, pk):
+        task = self.get_object(pk)
+        task.soft_deleted = not task.soft_deleted
+        task.save()
+        
+        serializer = RecurringTaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
